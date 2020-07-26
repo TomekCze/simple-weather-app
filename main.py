dd2540ec35bb2253b331bc1208b00a507b023df4
TomekCze <tomek.czernecki@gmail.com>
2020-07-26 22:26:27 +0200

    Add main function

---
+++ main.py
@@ -0,0 +1,28 @@
+from requests import get
+from json import loads
+from terminaltables import AsciiTable
+
+
+CITIES = ['Katowice', 'Łódź']
+
+
+def main():
+    url = 'https://danepubliczne.imgw.pl/api/data/synop'
+    response = get(url)
+    rows = [
+        ['Miasto', 'Godzina pomiaru', 'Temperatura']
+    ]
+
+    for row in loads(response.text):
+        if row['stacja'] in CITIES:
+            rows.append([
+                row['stacja'],
+                row['godzina_pomiaru'],
+                row['temperatura']
+            ])
+    table = AsciiTable(rows)
+    print(table.table)
+
+
+if __name__ == '__main__':
+    main()
